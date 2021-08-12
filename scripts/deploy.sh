3337b709eb98ad7f8dd570bbef2a1f57e6fab48e
mykeels <mykehell123@gmail.com>
2021-08-12 12:43:44 +0100

    fix: deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,3 +14,14 @@ git fetch --all
 git reset --hard origin/master
 yarn
 yarn build
+
+# (re)start pm2 instance
+PM2_APP_NAME=emotions
+pm2 describe $PM2_APP_NAME > /dev/null
+RUNNING=$?
+
+if [ "${RUNNING}" -ne 0 ]; then
+  pm2 start yarn --name $PM2_APP_NAME --interpreter bash -- serve -p $PORT
+else
+  pm2 restart $PM2_APP_NAME
+fi
